cf1a147794ca3831abead1b99adaf490b3633c74
04kelfos <kelseybetty@hotmail.com>
2024-01-04 11:41:39 +0900

    fixed errors

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,8 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function WeatherIcon(response) {
-  let icon = response.icon.data.weather[0].icon;
-  console.log(response.icon.data.weather[0].icon);
   return (
     <img alt="weather-icon" src="https://openweathermap.org/img/wn/01d.png" />
   );
